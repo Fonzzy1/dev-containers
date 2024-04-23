a487fed50c6ac047d5007cde41bcad275d80abf3
Fonzzy1 <alfiechadwick@hotmail.com>
2024-04-23 16:59:58 +1000

    Refactor Gist handling to use classes
    This commit refactors the existing script handling GitHub Gists.
    Functions were replaced with corresponding methods within three newly created classes: Vault, Secret, and Github.
    A Gist class was also added, providing cleaner encapsulation of Gist-related functionalities.
    This includes creating a new Gist instance from raw data and various Gist operations such as fetching comments and updating descriptions.
    The EmbeddingFinder class was updated to work with the introduced changes, and several redundant functions were removed.
    These changes provide better organization and clearer code representation, facilitating future development and maintenance.

--- run_scripts/gistnotes.py
+++ run_scripts/gistnotes.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from numpy.matrixlib import defmatrix
 from sentence_transformers import SentenceTransformer
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
@@ -6,44 +7,141 @@ from InquirerPy import inquirer
 import os
 import requests
 import tqdm
+from uuid import uuid4
 
 token = os.environ["GH_TOKEN"]
 headers = {"Authorization": "Token " + token}
 
 
-def list_all_gists(username):
-    url = f"https://api.github.com/users/{username}/gists"
-    response = requests.get(url, headers=headers)
-    gists = response.json()
-
-    ret_dict = {gist["id"]: {} for gist in gists}
-    print("Fetching Gists...")
-    for gist in tqdm.tqdm(gists, total=len(gists)):
-        ret_dict[gist["id"]] = process_gist(gist)
-    return ret_dict
+class Vault():
+    def __init__(self):
+        pass
 
 
-def process_gist(gist):
-    ret_dict = {}
-    ret_dict["description"] = (
-        gist["description"]
-        if gist["description"] != ""
-        else list(gist["files"].keys())[0]
-    )
-    ret_dict["files"] = list(gist["files"].keys())
-    return ret_dict
+
+class Secret():
+    def __init__(self, description, files, path):
+        self.description = description
+        self.files = files
+        self.path = path
+        self.embedding = None
+
+class Github:
+    def __init__(self, username):
+        self.user_name = username
+        self.gists = []
+        self.get_all_gists()
+
+    def get_all_gists(self):
+        url = f"https://api.github.com/users/{self.user_name}/gists"
+        response = requests.get(url, headers=headers)
+        raw_gists = response.json()
+
+        print("Fetching Gists...")
+        for raw_gist in tqdm.tqdm(raw_gists, total=len(raw_gists)):
+            self.gists.append(Gist.from_raw(raw_gist))
 
 
-def get_single_gist(gist_id):
-    url = f"https://api.github.com/gists/{gist_id}"
-    response = requests.get(url, headers=headers)
-    gist = response.json()
+class Gist:
+    def __init__(self, id, description, files):
+        self.id = id
+        self.description = description
+        self.files = files
+        self.embedding = None
 
-    return process_gist(gist)
+    # Using a class method to create a new Gist instance from raw data
+    @classmethod
+    def from_raw(cls, raw_dict):
+        description = (
+            raw_dict["description"]
+            if raw_dict["description"] != ""
+            else list(raw_dict["files"].keys())[0]
+        )
+        files = list(raw_dict["files"].keys())
+        id = raw_dict["id"]
+        return cls(id, description, files)
+
+    @classmethod
+    def from_id(cls, id):
+        url = f"https://api.github.com/gists/{id}"
+        response = requests.get(url, headers=headers)
+        raw_gist = response.json()
+        return cls.from_raw(raw_gist)
+
+    def make_new_comment(self, new_comments_content):
+        requests.post(
+            f"https://api.github.com/gists/{self.id}/comments",
+            json={"body": new_comments_content},
+            headers=headers,
+        )
+        print("Comment Saved")
+
+    def update_gist_desc(self, new_description):
+        r = requests.patch(
+            f"https://api.github.com/gists/{self.id}",
+            json={"description": new_description},
+            headers=headers,
+        )
+        print("Description Updated")
+
+    def get_comments(self):
+        raw_comments = requests.get(
+            f"https://api.github.com/gists/{self.id}/comments", headers=headers
+        ).json()
+        comments = [
+            {
+                "user": x["user"]["login"],
+                "comment": x["body"],
+                "created": x["created_at"],
+            }
+            for x in raw_comments
+        ]
+        out_string = ""
+        for comment in comments:
+            comment_string = f""" **>> {comment['user']} @ {comment['created']}**\n ---------------------------------------------\n{comment['comment']}\n"""
+            out_string += comment_string
+
+        out_string += "\n ---------------------------------------------\n"
+        return out_string
+
+    def open(self):
+        comments = self.get_comments()
+        os.system(f" gh gist clone {self.id} /gist > /dev/null 2>&1;")
+        with open("/gist/.comments.md", "w") as f:
+            f.write(comments)
+        with open("/gist/.description.txt", "w") as f:
+            f.write(self.description)
+
+        os.system(f"vim")
+
+        new_comments_content = (
+            open("/gist/.comments.md", "r")
+            .read()
+            .split("---------------------------------------------")[-1]
+            .lstrip()
+        )
+        if new_comments_content != "":
+            self.make_new_comment( new_comments_content)
+
+        new_description = open("/gist/.description.txt", "r").read().rstrip().lstrip()
+        if new_description != self.description:
+            self.update_gist_desc( new_description)
+            self.description = new_description
+
+        os.remove("/gist/.comments.md")
+        os.remove("/gist/.description.txt")
+        _ = os.system(
+            f"""
+            git add . > /dev/null 2>&1;
+            git commit -m "update" > /dev/null 2>&1;
+            git push > /dev/null 2>&1;
+            find /gist -mindepth 1 -delete > /dev/null 2>&1;
+            """
+        )
 
 
 class EmbeddingFinder:
-    def __init__(self, data: dict):
+    def __init__(self, data: list):
         self.data = data
         print("Loading Model...")
         self.model = SentenceTransformer(
@@ -51,19 +149,19 @@ class EmbeddingFinder:
         )  # Use a light model for quick embedding
 
     def prepare_data(self):
-        texts = [x["description"] for x in self.data.values()]
+        texts = [x.description for x in self.data]
         print("Preparing data...")
         raw_embeddings = self.model.encode(
             texts, convert_to_tensor=True, show_progress_bar=True
         )
 
-        for i, k in enumerate(self.data.keys()):
-            self.data[k]["embedding"] = raw_embeddings[i].detach().numpy()
+        for i , _ in enumerate(self.data):
+            self.data[i].embedding = raw_embeddings[i].detach().numpy()
 
     def find(self, query):
         query_embedding = self.model.encode([query], convert_to_tensor=True)
 
-        embeddings_numpy = np.array([x["embedding"] for x in self.data.values()])
+        embeddings_numpy = np.array([x.embedding for x in self.data])
 
         cos_similarities = cosine_similarity(
             query_embedding.detach().numpy(), embeddings_numpy
@@ -73,28 +171,21 @@ class EmbeddingFinder:
         # hence we will want to reverse the order (argsort returns from smallest to largest)
         top_similar_indices = cos_similarities.argsort()[::-1]
 
-        return [list(self.data.keys())[i] for i in top_similar_indices]
+        return [self.data[i] for i in top_similar_indices]
 
-    def update_embedding_for_key(self, key):
-        if key in self.data:
-            text = self.data[key]["description"]
-            embedding = (
-                self.model.encode([text], convert_to_tensor=True)[0].detach().numpy()
-            )
-            self.data[key]["embedding"] = embedding
+    def get_item_from_key(self, key):
+      for item in self.data:
+        if item.description == key:
+          return item
+      return None
 
 
-def get_key_from_description(data, target_description):
-    for key, value in data.items():
-        if "description" in value and value["description"] == target_description:
-            return key
-    return None
 
 
 def run_gist_notes(username):
 
-    data = list_all_gists(username)
-    finder = EmbeddingFinder(data)
+    hub = Github(username)
+    finder = EmbeddingFinder(hub.gists)
     finder.prepare_data()
 
     try:
@@ -106,9 +197,9 @@ def run_gist_notes(username):
 
             while True:
 
-                keys = finder.find(query)
+                items = finder.find(query)
 
-                choices = [finder.data[x]["description"] for x in keys] + [
+                choices = [x.description for x in items] + [
                     "? (Return to search)",
                     "+ (Make New Gist)",
                 ]
@@ -146,78 +237,6 @@ def run_gist_notes(username):
 
 def run_vim(gist_id, description):
 
-    comments = get_comments(gist_id)
-    os.system(f" gh gist clone {gist_id} /gist > /dev/null 2>&1;")
-    with open("/gist/.comments.md", "w") as f:
-        f.write(comments)
-    with open("/gist/.description.txt", "w") as f:
-        f.write(description)
-
-    os.system(f"vim")
-
-    new_comments_content = (
-        open("/gist/.comments.md", "r")
-        .read()
-        .split("---------------------------------------------")[-1]
-        .lstrip()
-    )
-    if new_comments_content != "":
-        make_new_comment(gist_id, new_comments_content)
-
-    new_description = open("/gist/.description.txt", "r").read().rstrip().lstrip()
-    if new_description != description:
-        update_gist_desc(gist_id, new_description)
-
-    os.remove("/gist/.comments.md")
-    os.remove("/gist/.description.txt")
-    _ = os.system(
-        f"""
-        git add . > /dev/null 2>&1;
-        git commit -m "update" > /dev/null 2>&1;
-        git push > /dev/null 2>&1;
-        find /gist -mindepth 1 -delete > /dev/null 2>&1;
-        """
-    )
-    _ = print(f"https://gist.github.com/{gist_id}")
-
-
-def make_new_comment(gist_id, new_comments_content):
-    requests.post(
-        f"https://api.github.com/gists/{gist_id}/comments",
-        json={"body": new_comments_content},
-        headers=headers,
-    )
-    print("Comment Saved")
-
-
-def update_gist_desc(gist_id, new_description):
-    r = requests.patch(
-        f"https://api.github.com/gists/{gist_id}",
-        json={"description": new_description},
-        headers=headers,
-    )
-    print("Description Updated")
-
-
-def get_comments(gist_id):
-    raw_comments = requests.get(
-        f"https://api.github.com/gists/{gist_id}/comments", headers=headers
-    ).json()
-    comments = [
-        {
-            "user": x["user"]["login"],
-            "comment": x["body"],
-            "created": x["created_at"],
-        }
-        for x in raw_comments
-    ]
-    out_string = ""
-    for comment in comments:
-        comment_string = f""" **>> {comment['user']} @ {comment['created']}**\n ---------------------------------------------\n{comment['comment']}\n"""
-        out_string += comment_string
-
-    out_string += "\n ---------------------------------------------\n"
-    return out_string
 
 
 if __name__ == "__main__":
