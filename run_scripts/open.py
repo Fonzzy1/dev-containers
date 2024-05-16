afb83fb158614eb11ed992a394c8db4840ee325b
Fonzzy1 <alfiechadwick@hotmail.com>
2024-05-16 15:16:10 +1000

    Added argparse to handle path/reader flag
    This commit introduces the argparse module to better handle command line
    arguments for the open.py script.
    
    It replaces the manual sys.argv method previously used for obtaining the
    path to be processed.
    
    The script now also accepts a --reader flag to modify URLs.
    
    Instead of directly executing the path-to-command functionality in the
    main part of the script, we now execute a main function that entails
    handling of the argparser and the path to command operation.

--- run_scripts/open.py
+++ run_scripts/open.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import subprocess
 import os
-from urllib.parse import urlparse
 import sys
+from urllib.parse import urlparse
+import argparse
 
 
 def is_url(path):
@@ -13,9 +13,12 @@ def is_url(path):
         return False
 
 
-def path_to_command(path):
+def path_to_command(path, reader_flag):
     if is_url(path):
-        return f'xdg-open "{path}"'
+        if reader_flag:
+            return f'xdg-open "about:reader?url={path}"'
+        else:
+            return f'xdg-open "{path}"'
 
     sys_dir = os.environ["SYS_DIR"]
     cont_dir = os.environ["CONT_DIR"]
@@ -23,28 +26,34 @@ def path_to_command(path):
     sys_vault = os.environ["SYS_VAULT"]
     cont_vault = os.environ["VAULT"]
 
-    if path == None:
+    if path is None:
         return f'xdg-open "{sys_dir}"'
-
     elif path.startswith(cont_dir):
         path = path.replace(cont_dir, sys_dir)
         return f'xdg-open "{path}"'
-
     elif path.startswith(cont_vault):
         path = path.replace(cont_vault, sys_vault)
         return f'xdg-open "{path}"'
-
     else:
         os.system(
             f"echo 'Path {path} is not within the container directory {cont_dir} or vault dir {cont_vault}. Skipping operation.' >> /root/.xgd_log.txt"
         )  # Adds command to log.txt
 
 
-if __name__ == "__main__":
-    try:
-        path = sys.argv[1]
-    except:
-        path = None
-    command = path_to_command(sys.argv[1])
+def main():
+    parser = argparse.ArgumentParser(description="Process some inputs.")
+    parser.add_argument("path", type=str, help="The path to be processed.")
+    parser.add_argument(
+        "--reader",
+        action="store_true",
+        help='Appends "about:reader?url=" to the start of the path if it is a url',
+    )
+    args = parser.parse_args()
+
+    command = path_to_command(args.path, args.reader)
     os.system(f"echo '{command}' >> /root/.xgd_log.txt")  # Adds command to log.txt
     os.system(f"echo '{command}' > /pipe")
+
+
+if __name__ == "__main__":
+    main()
