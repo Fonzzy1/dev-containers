1d874daa347490b82ad54638769dfd2ede45acda
Fonzzy1 <alfiechadwick@hotmail.com>
2025-09-17 12:26:00 +1000

    Tidy up the tree config

--- vim/lua/tree_config.lua
+++ vim/lua/tree_config.lua
@@ -77,29 +77,29 @@ require 'nvim-treesitter.configs'.setup {
         disable = { 'aichat' },
         additional_vim_regex_highlighting = false,
     },
-    playground = { enable = true }, -- Fixed the syntax here
+    playground = { enable = false }, -- Fixed the syntax here
 
 }
 
--- vim.api.nvim_create_autocmd("FileType", {
---     pattern = "aichat",
---     callback = function()
---         local query = require("vim.treesitter.query")
---         local parser_config = vim.treesitter.language.get_lang("markdown")
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "aichat",
+    callback = function()
+        local query = require("vim.treesitter.query")
+        local parser_config = vim.treesitter.language.get_lang("markdown")
 
---         -- Get the existing highlight query for markdown
---         local existing = query.get_query("markdown", "highlights")
---         local existing_str = tostring(existing)
+        -- Get the existing highlight query for markdown
+        local existing = query.get_query("markdown", "highlights")
+        local existing_str = tostring(existing)
 
---         -- Add your custom rule to highlight lines starting with ">>>"
---         local custom = [[
---       ((paragraph) @comment (#match? @comment "^>>>"))
---     ]]
+        -- Add your custom rule to highlight lines starting with ">>>"
+        local custom = [[
+      ((paragraph) @comment (#match? @comment "^>>>"))
+    ]]
 
---         -- Merge and set the new combined query
---         query.set("markdown", "highlights", existing_str .. "\n" .. custom)
---     end,
--- })
+        -- Merge and set the new combined query
+        query.set("markdown", "highlights", existing_str .. "\n" .. custom)
+    end,
+})
 
 
 require('mini.ai').setup()
