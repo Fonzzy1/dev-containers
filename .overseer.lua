28cc0770173933a61539ac7acd462d3f0ccad9bb
Fonzzy1 <alfiechadwick@hotmail.com>
2025-09-22 10:29:59 +1000

    add in function for docker login

--- .overseer.lua
+++ .overseer.lua
@@ -9,4 +9,17 @@ return {
             }
         end,
     },
+    {
+        name = "Login to Docker",
+        desc = "Log in to Docker Hub",
+        builder = function(_)
+            return {
+                cmd = { "docker login -u fonzzy1" },
+                components = {
+                    { "open_output", direction = "float", on_start = "always", focus = true },
+                    "default"
+                }
+            }
+        end,
+    }
 }
