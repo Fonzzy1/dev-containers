5408bb3b38e5b2645192b25ab3d546f535ae666b
Fonzzy1 <alfiechadwick@hotmail.com>
2025-10-08 09:45:04 +1100

    Better default for the notes

--- run_scripts/note_default.py
+++ run_scripts/note_default.py
@@ -5,41 +5,16 @@ import os
 import re
 import argparse
 
+default_template = """---
+title: {name}
+author: Alfie Chadwick
+date: "{date}"
+"""
+
 def main():
-    default_template = """---
-title: {name}
-author: Alfie Chadwick
-date: "{date}"
-filters:
-    - /scripts/callouts.lua
----
-"""
-
-    reviews_template = """---
-title: {name}
-author: Alfie Chadwick
-date: "{date}"
-title:
-type:
-artist:
-rating:
-cover:
-while:
-on:
-review: >
-
----
-"""
-
     parser = argparse.ArgumentParser()
     parser.add_argument("file", metavar="F", type=str, help="file to check")
-
     args = parser.parse_args()
-
-    # Check if path contains the 'reviews' directory
-    if "/wiki/Website/content/reviews" in args.file.replace("\\", "/"):
-        template = reviews_template
-    else:
         template = default_template
 
     # get the filename along with extension
@@ -47,7 +22,7 @@ review: >
     # strip the extension to get only the name
     name, ext = os.path.splitext(filename_with_ext)
 
-    print(template.format(name=name, date=date.today().strftime("%Y-%m-%d")))
+    print(template.format(name=name.replace("_"," ").replace("-", " "), date=date.today().strftime("%Y-%m-%d")))
 
 if __name__ == '__main__':
     main()
