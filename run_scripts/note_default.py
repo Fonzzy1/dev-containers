22522fcde9606211dfe25e2694827b8f1dd259ab
Fonzzy1 <alfiechadwick@hotmail.com>
2025-10-14 10:44:53 +1100

    fix template quarto

--- run_scripts/note_default.py
+++ run_scripts/note_default.py
@@ -11,18 +11,25 @@ author: Alfie Chadwick
 date: "{date}"
 """
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", metavar="F", type=str, help="file to check")
     args = parser.parse_args()
-        template = default_template
+    template = default_template
 
     # get the filename along with extension
     filename_with_ext = os.path.basename(args.file)
     # strip the extension to get only the name
     name, ext = os.path.splitext(filename_with_ext)
 
-    print(template.format(name=name.replace("_"," ").replace("-", " "), date=date.today().strftime("%Y-%m-%d")))
+    print(
+        template.format(
+            name=name.replace("_", " ").replace("-", " "),
+            date=date.today().strftime("%Y-%m-%d"),
+        )
+    )
+
 
 if __name__ == '__main__':
     main()
