f8a4441f03dc824b2e990bd39911b89754dceb98
Fonzzy1 <alfiechadwick@hotmail.com>
2025-10-15 10:54:56 +1100

    New overseer templates

--- vim/lua/overseer_templates.lua
+++ vim/lua/overseer_templates.lua
@@ -27,6 +27,34 @@ require("overseer").register_template({
 })
 
 require("overseer").register_template({
+    name = "Docker Build",
+    builder = function()
+        local current_dir = vim.fn.expand('%:p:h')
+        return {
+            cmd = { "docker", "build", "-f", vim.fn.expand('%:p'), current_dir },
+            components = { "default" },
+        }
+    end,
+    condition = {
+        filetype = { "dockerfile" },
+    },
+})
+
+require("overseer").register_template({
+    name = "Docker Run Current",
+    builder = function()
+        local current_file = vim.fn.expand('%:p')
+        return {
+            cmd = { "quarto", "render", current_file },
+            components = { "default" },
+        }
+    end,
+    condition = {
+        filetype = { "dockerfile" },
+    },
+})
+
+require("overseer").register_template({
     name = "Open Shell",
     builder = function(_)
         return {
@@ -37,4 +65,50 @@ require("overseer").register_template({
             }
         }
     end,
+})
+
+require("overseer").register_template({
+    name = "Docker Compose Up",
+    builder = function(_)
+        return {
+            cmd = { "docker", "compose", "up", "--build" },
+            components = {
+                { "open_output", direction = "float", on_start = "always", focus = true },
+                "default"
+            }
+        }
+    end,
+    condition = {
+        callback = function(search)
+            local docker_compose_path = vim.fn.getcwd() .. "/docker-compose.yml"
+            local file = io.open(docker_compose_path, "r")
+            if file then
+                io.close(file)
+                return true
+            else
+                return false
+            end
+        end,
+    },
+})
+
+require("overseer").register_template({
+    name = "Install Python Requirements",
+    builder = function(_)
+        return {
+            cmd = { "pip", "install", '-r', 'requirements.txt' },
+        }
+    end,
+    condition = {
+        callback = function(search)
+            local requirements_path = vim.fn.getcwd() .. "/requirements.txt"
+            local file = io.open(requirements_path, "r")
+            if file then
+                io.close(file)
+                return true
+            else
+                return false
+            end
+        end,
+    },
 })
