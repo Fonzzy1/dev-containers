5b06e55c45a1dfb85a20885c2479cc26d88408e2
Fonzzy1 <alfiechadwick@hotmail.com>
2026-04-24 11:34:26 +1000

    New vis config + vsplit

--- vim/lua/visual_config.lua
+++ vim/lua/visual_config.lua
@@ -121,7 +121,7 @@ require("noice").setup({
         command_palette = true,       -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false,       -- add a border to hover docs and signature help
+      lsp_doc_border = false,       -- add a border to hover docs and signature help
     },
 })
 require('gitsigns').setup {
@@ -145,7 +145,7 @@ require('gitsigns').setup {
         use_focus = true,
     },
     current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
-    sign_priority                = 6,
+    sishshallahign_priority                = 6,
     update_debounce              = 100,
     status_formatter             = nil,   -- Use default
     max_file_length              = 40000, -- Disable if file is longer than this (in lines)
@@ -172,7 +172,6 @@ _G.SmartVsplit = function(filename_or_entry)
     elseif type(filename_or_entry) == "table" then
         -- Telescope-style entry table - derive path from common fields
         local entry = filename_or_entry
-
         -- Extract file path from common Telescope fields (priority: path > filename > uri > value)
         filename = entry.path or entry.filename
         if not filename and entry.uri then
@@ -215,7 +214,7 @@ _G.SmartVsplit = function(filename_or_entry)
         local full_path = vim.fn.fnamemodify(bufname, ":p")
         local basename = vim.fn.fnamemodify(bufname, ":t")
 
-        if full_path == target_file or basename == target_basename then
+        if full_path == target_file then
             found_bufnr = bufnr
             found_winid = winid
             break
