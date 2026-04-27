1427bd53fe8fba3eef3778eba2b832276e059930
Fonzzy1 <alfiechadwick@hotmail.com>
2026-04-27 11:31:09 +1000

    Return to sync buffer format

--- vim/lua/lsp.lua
+++ vim/lua/lsp.lua
@@ -144,6 +144,6 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     group = augroup,
     buffer = bufnr,
     callback = function()
-        vim.lsp.buf.format({ async = true })
+        vim.lsp.buf.format({ async = false })
     end,
 })
