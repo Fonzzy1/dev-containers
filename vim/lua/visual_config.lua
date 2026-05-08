d45b9fda27743b79643fab0e348fbf11f2f661a7
Fonzzy1 <alfiechadwick@hotmail.com>
2026-05-08 13:07:32 +1000

    Nicer i3

--- vim/lua/visual_config.lua
+++ vim/lua/visual_config.lua
@@ -145,7 +145,7 @@ require('gitsigns').setup {
         use_focus = true,
     },
     current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
-    sign_priority      = 6,
+    sign_priority                = 6,
     update_debounce              = 100,
     status_formatter             = nil,   -- Use default
     max_file_length              = 40000, -- Disable if file is longer than this (in lines)
@@ -245,3 +245,6 @@ end, {
     complete = "file",
     desc = "Smart vsplit: jump to existing buffer or open in vertical split"
 })
+
+
+require("catppuccin").setup({ transparent_background = true })
